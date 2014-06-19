b9b68049702f888a9b274ad2fd10a72076a6f21c
Robbie Ostrow <rostrow@iqt.org>
2014-06-19 16:20:33 -0700

    Python: optimal_count now returns the optimal count after it is calculated.

--- interfaces/python/igraph/clustering.py
+++ interfaces/python/igraph/clustering.py
@@ -970,6 +970,7 @@ class VertexDendrogram(Dendrogram):
         this method calculates the optimal number of clusters by maximizing
         the modularity along all the possible cuts in the dendrogram.
         """
+        print "checking"
         if self._optimal_count is not None:
             return self._optimal_count
 
@@ -982,6 +983,7 @@ class VertexDendrogram(Dendrogram):
                 optimal_count = n-step
                 max_q = q
         self._optimal_count = optimal_count
+        return self._optimal_count
 
     @optimal_count.setter
     def optimal_count(self, value):
